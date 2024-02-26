9c0c8f1f126e1d6035e174ac039e57ba17c78999
suresh <8SURESHGHOSH@GMAIL.COM>
2024-02-26 20:45:06 +0530

    dice update

---
+++ fun-argument.js
@@ -0,0 +1,17 @@
+function printInfo(name,age){
+    console.log(`${name}'s age is ${age}`);
+}
+printInfo("suresh",23);
+printInfo("sujoy",24);
+//second example
+function sum(a,b){
+    console.log(a+b);
+}
+sum(1,2);
+sum(145,564);
+//gives us the average of 3 numbers
+function calAvg(a,b,c){
+    let Avg=(a+b+c)/3;
+    console.log(Avg);
+}
+calAvg(2,4,4);
