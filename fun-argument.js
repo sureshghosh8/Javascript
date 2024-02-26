3243a22429c4df36c6fd656eefb5a4d19ed733b5
suresh <8SURESHGHOSH@GMAIL.COM>
2024-02-26 22:06:33 +0530

    functions

--- fun-argument.js
+++ fun-argument.js
@@ -11,7 +11,66 @@ sum(1,2);
 sum(145,564);
 //gives us the average of 3 numbers
 function calAvg(a,b,c){
-    let Avg=(a+b+c)/3;
-    console.log(Avg);
+    let Avg=Math.floor((a+b+c)/3);
+    console.log(Avg,"this is agverage");
 }
 calAvg(2,4,4);
+calAvg(2,2,1);
+//prints the multiplication of tables
+function printTable(n){
+    for (let i=n;i<=n*10;i+=n){
+        console.log(i);
+    }
+}
+printTable(10);
+//return function
+function add(a,b){
+    return a+b;
+}
+console.log(sum(sum(4,5),5));
+//that returns the sum of numbers from 1 to n
+
+function getSum(n){
+    let sum=0;
+for(let i=1;i<=n;i++){
+    sum=sum+i;
+}
+return sum;
+}
+//concatenation of all strings in an array
+let str=["hi","hello","bye","!"];
+function concat(str){
+    let result="";
+    for(let i=0;i<str.length;i++){
+        result+=str[i];
+    }
+    return result;
+}
+//function Scope
+let num=54;//global Scope
+function calSum(a,b){
+    let Sum=a+b;//function Scope
+    console.log(Sum);
+}
+calSum(1,2);
+//block scope
+for(let i=1;i<=5;i++){
+    console.log(i);//block scope
+}
+console.log(i);
+//lexical scope
+function outerFun(){
+    let x=5;
+    let y=6;
+    function innerFun(){
+        let a=10;
+        console.log(x);
+        console.log(y);
+
+    }
+    console.log(a);
+}
+//function Expressions
+var sum=function(a,b){
+    return a+b;
+}
