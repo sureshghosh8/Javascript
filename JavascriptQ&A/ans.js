8f8ad40bf393d4644b90f7b3d68e3fb80443ad33
suresh <8SURESHGHOSH@GMAIL.COM>
2024-03-11 15:22:43 +0530

    FaCtoRial

--- JavascriptQ&A/ans.js
+++ JavascriptQ&A/ans.js
@@ -234,3 +234,12 @@ while(copy>0){
     copy=Math.floor(copy/10);
 }
 console.log(sum);
+//Print the factorial of a number n.
+//[Factorial of a number n is the product of all positive integers less than or equal to a   given positive integer and denoted by that integer. ]
+let n=5;
+let factorial=1;
+for(let i=1; i<=n; i++){
+    factorial *=i;
+}
+console.log(`factorial of ${n} is ${factorial}`);
+  
