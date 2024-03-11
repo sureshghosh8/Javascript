877052b5010bda6c2f1a4c63a8731a6be11f4634
suresh <8SURESHGHOSH@GMAIL.COM>
2024-03-11 15:19:20 +0530

    the-sum-of-digits

--- JavascriptQ&A/ans.js
+++ JavascriptQ&A/ans.js
@@ -213,3 +213,24 @@ for(let i=0;i<arr.length;i++){
     }
 }
 console.log(arr);
+//Qs2. Write a JS program to find the no of digits in a number.
+//Example : if number = 287152, count = 6
+let number=287152;
+let count=0;
+let copy=number;
+while(copy>0){
+    count++;
+    copy=Math.floor(copy/10);
+}
+console.log(count);
+//Qs3. Write a JS program to find the sum of digits in a number.
+//Example : if number = 287152, sum = 25
+let number=25415;
+let sum=0;
+let copy=number;
+while(copy>0){
+    digit=copy%10;
+    sum+=digit;
+    copy=Math.floor(copy/10);
+}
+console.log(sum);
