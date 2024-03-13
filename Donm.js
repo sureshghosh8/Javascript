510049c42f45d7e15ff693cd1c31152c8b025299
suresh <8SURESHGHOSH@GMAIL.COM>
2024-03-13 10:42:52 +0530

    DOM

---
+++ Donm.js
@@ -0,0 +1,4 @@
+console.dir(document.querySelector("h1"));
+console.dir(document.querySelector("#discription"));
+console.dir(document.querySelector(".oldimage"));
+console.dir(document.querySelectorAll("div a"));
