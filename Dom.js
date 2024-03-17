daf4f2e0c435b1e5e29852d826e927483e16a292
suresh <8SURESHGHOSH@GMAIL.COM>
2024-03-17 21:18:53 +0530

    extract-form-value

--- Dom.js
+++ Dom.js
@@ -41,3 +41,11 @@ inp.addEventListener("keydown",function(e){
 
   console.log("key was pressed");
 })
+//extract-form data
+let form=document.querySelector("form");
+form.addEventListener("submit",function(){
+  let user=document.querySelector("#user");
+  let pass=document.querySelector("#pass")
+  console.log(user.value);
+  console.log(pass.value);
+})
